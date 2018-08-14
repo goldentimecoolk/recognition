68be9113ad17ec4062ba092ffbf933bab0c6526e
goldentimecoolk <39372277+goldentimecoolk@users.noreply.github.com>
2018-08-14 16:18:29 +0800

    Update test.py
    Add a function to achieve various accuracy evaluation methods easily, such as top 1, top 5, or top k.

--- test.py
+++ test.py
@@ -18,27 +18,34 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 plt.ion()   # interactive mode
 
-data_transforms = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
 
-data_dir = '/home/jsk/s/prcv/dataset/v2'
-image_datasets = datasets.ImageFolder(os.path.join(data_dir, 'rest_test'), data_transforms)
-testloader = torch.utils.data.DataLoader(image_datasets, batch_size=512, shuffle=False, num_workers=32)
+root = '/home/jsk/s/torch/params'
+model_dir = 'resnet50'
+param_str = '50_8'
+
+
+def main():
+    data_transforms = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+    data_dir = '/home/jsk/s/prcv/dataset/v2'
+    image_datasets = datasets.ImageFolder(os.path.join(data_dir, 'rest_test'), data_transforms)
+    testloader = torch.utils.data.DataLoader(image_datasets, batch_size=512, shuffle=False, num_workers=32)
               
-dataset_sizes = len(image_datasets)
-class_names = image_datasets.classes
+    dataset_sizes = len(image_datasets)
+    class_names = image_datasets.classes
 
-device = torch.device("cuda:6" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:6" if torch.cuda.is_available() else "cpu")
 
-model = {'resnet50':models.resnet50(pretrained=False), 
-	'resnet101':models.resnet101(pretrained=False), 
-	'resnet152':models.resnet152(pretrained=False)}
+    model = {'resnet50':models.resnet50(pretrained=False), 
+	    'resnet101':models.resnet101(pretrained=False), 
+	    'resnet152':models.resnet152(pretrained=False)}
 
-param = {'101_64':'resnet101_epoch20_ft_batch64.pkl',
+    param = {'101_64':'resnet101_epoch20_ft_batch64.pkl',
 	'101_32':'resnet101_epoch20_ft_batch32.pkl',
 	'101_16':'resnet101_epoch26_ft_batch16.pkl',
 	'101_8':'resnet101_epoch26_ft_batch8.pkl',
@@ -53,37 +60,59 @@ param = {'101_64':'resnet101_epoch20_ft_batch64.pkl',
 	'50_2':'resnet50_epoch30_ft_batch2.pkl',
 	'50_1':'resnet50_epoch30_ft_batch1.pkl'}
 
-root = '/home/jsk/s/torch/params'
+    model_test = model[model_dir]
+    num_ftrs = model_test.fc.in_features
+    model_test.fc = nn.Linear(num_ftrs, 205)
+    print(model_test)
+    print('test param %s of model %s' % (param[param_str],model_dir))
 
-model_dir = 'resnet101'
-param_str = '101_2'
+    param_dir = os.path.join(root,param[param_str])
+    model_test.load_state_dict(torch.load(param_dir))
+    model_test = model_test.to(device)
+    model_test.eval()
 
-model_test = model[model_dir]
-num_ftrs = model_test.fc.in_features
-model_test.fc = nn.Linear(num_ftrs, 205)
-print(model_test)
-print('test param %s of model %s' % (param[param_str],model_dir))
+    correct = 0
+    correct_1 = 0
+    correct_5 = 0
+    top1 = 0
+    top5 = 0
+    total = 0
+    batch = 0
 
-param_dir = os.path.join(root,param[param_str])
-model_test.load_state_dict(torch.load(param_dir))
-model_test = model_test.to(device)
-model_test.eval()
+    with torch.no_grad():
+        for inputs, labels in testloader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model_test(inputs)
 
+            #_, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            #correct += (predicted == labels).sum().item()
+        
+            dcorrect_1, dcorrect_5 = accuracy(outputs,labels,topk=(1,5))
+            correct_1 += dcorrect_1
+            correct_5 += dcorrect_5
+            top1 = correct_1.float()/total
+            top5 = correct_5.float()/total
 
-correct = 0
-total = 0
-batch = 0
-with torch.no_grad():
-    for inputs, labels in testloader:
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-        outputs = model_test(inputs)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-        batch += 1
-        print('batch %d accuracy: %.3f %%' % (batch,100.*correct/total))
-print('Accuracy of the %s on the %d test images: %.3f %%' % (param[param_str],total,100 * correct / total))
+            batch += 1
+	
+            print('batch %d top1 accuracy: %.3f %% top5 accuracy: %.3f %%' % (batch,100*top1,100*top5))
+    print('Accuracy of the %s on the %d test images: top1 %.3f %%  top5 %.3f %%' % (param[param_str],total,100*top1,100*top5))
 
+def accuracy(output, target, topk=(1,)):
+    
+    maxk = max(topk)
+    batch_size = target.size(0)
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
 
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k)
+    return res
 
+if __name__ == '__main__':
+    main()
